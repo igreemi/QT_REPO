3f79a9bb31317540edfcbc120947c0ffcb411438
Abramov Maxim <greem@yandex.ru>
2023-04-30 22:06:29 +0300

    Delete Build

--- Core_Qt_Modules/Task1/Build/Debug/CMakeFiles/Task1.dir/Task1_autogen/mocs_compilation.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Task1.dir/Task1_autogen/mocs_compilation.cpp.obj: \
- C:\kurs_cpp\Home_Work_CPP\Qt\Core_Qt_Modules\Task1\Build\Debug\Task1_autogen\mocs_compilation.cpp
--- QT_Designer/Build/Debug/CMakeFiles/QT_Designer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for QT_Designer.
-# This may be replaced when dependencies are built.
