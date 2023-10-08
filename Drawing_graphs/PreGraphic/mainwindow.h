d0d96e25bda40209712415e4b995d60b0a927ccd
Abramov Maxim <greem@yandex.ru>
2023-10-08 17:12:03 +0300

    Add Drawing_graphs/

---
+++ Drawing_graphs/PreGraphic/mainwindow.h
@@ -0,0 +1,72 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QDataStream>
+#include <QMessageBox>
+#include <QtGlobal>
+#include <QtConcurrent>
+#include <algorithm>
+
+///Подключаем все что нужно для графиков
+#include "graphicchart.h"
+#include <QLineSeries>
+#include <QtCharts>
+#include <QChartView>
+
+#define NUM_GRAPHS 1
+
+#define FD 1000.0 //частота дискретизации
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    //Метод считывает данные из исходного файла
+    QVector<uint32_t> ReadFile(QString path, uint8_t numberChannel);
+    //Метод преобразует данные физические величины, В?
+    QVector<double> ProcessFile(QVector<uint32_t> dataFile);
+    //Метод ищет Максимумы
+    QVector<double> FindMax(QVector<double> resultData);
+    //Метод ищет минимумы
+    QVector<double> FindMin(QVector<double> resultData);
+    //Метод отображает результаты
+    void DisplayResult(QVector<double> mins, QVector<double> maxs);
+
+
+
+private slots:
+    void on_pb_path_clicked();
+    void on_pb_start_clicked();
+
+
+
+    void on_pb_clearResult_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QString pathToFile = "";
+    uint8_t numberSelectChannel = 0xEA;
+
+    QVector<uint32_t> readData;
+    QVector<double> procesData;
+    QVector<double> mins, maxs;
+
+    void BuildAGraph(QVector<double> res, double min, double max, double st);
+    void ViewGraph(void);
+
+    GraphicChart* graphClass;
+    QChart* chart;
+    QChartView* chartView;
+    QGridLayout *layout;
+
+};
+#endif // MAINWINDOW_H
